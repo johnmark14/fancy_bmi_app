45045331976a3b905ce68c800d3c299f689d374c
johnmark14 <johmarkgabrentina@gmail.com>
2020-12-05 00:38:03 +0800

    setup app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,16 @@
 import 'package:flutter/material.dart';
+import './input_page.dart';
 
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        home: InputPage(),
-      );
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => null,
-        child: Icon(Icons.add),
+    return MaterialApp(
+      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0a0e21),
       ),
     );
   }
